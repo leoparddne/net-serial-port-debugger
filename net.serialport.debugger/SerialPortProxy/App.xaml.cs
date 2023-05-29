fdd8bf784326173a79a6d8cd92ec520257315294
ives <renhanlinbsl@163.com>
2023-05-30 00:50:22 +0800

    预留基础项目结构

---
+++ net.serialport.debugger/SerialPortProxy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SerialPortProxy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
