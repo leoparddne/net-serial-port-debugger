f75fefb9b980b9307c62928ecd7a57296bbd3753
ives <renhanlinbsl@163.com>
2023-05-30 21:17:01 +0800

    调整服务承载模式

---
+++ net.serialport.debugger/SerialPortProxyService/Program.cs
@@ -0,0 +1,10 @@
+using SerialPortProxyService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
