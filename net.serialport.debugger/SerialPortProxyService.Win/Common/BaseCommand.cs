c39e13a95a53f1bbfd9f2d9f919306158ec99a68
ives <renhanlinbsl@163.com>
2023-07-12 10:32:26 +0800

    WIP

---
+++ net.serialport.debugger/SerialPortProxyService.Win/Common/BaseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SerialPortProxyService.Win.Common
+{
+    public class BaseCommand : ICommand
+    {
+        public BaseCommand(Action<object> executeAction)
+        {
+            ExecuteAction = executeAction;
+        }
+
+        public Action<object> ExecuteAction { get; set; }
+
+
+        public Func<object, bool> CanExecuteAction { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteAction != null)
+                return CanExecuteAction(parameter);
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            ExecuteAction?.Invoke(parameter);
+        }
+    }
+}
