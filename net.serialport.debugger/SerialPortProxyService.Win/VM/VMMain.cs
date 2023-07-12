1bc4b9b000731131c69cf08d9fdc29e10f6d2f89
ives <renhanlinbsl@163.com>
2023-07-12 11:35:33 +0800

    加载系统串口

--- net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
+++ net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
@@ -1,14 +1,26 @@
 ﻿using PropertyChanged;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
 
 namespace SerialPortProxyService.Win.VM
 {
     [AddINotifyPropertyChangedInterface]
     public class VMMain
     {
+        public ObservableCollection<string> SerialPortList { get; set; }
+
+        /// <summary>
+        /// 选中的串口
+        /// </summary>
+        public string SelectSerialPort { get; set; }
+
+        public VMMain()
+        {
+            var sysSerialPortList = SerialPort.GetPortNames();
+            if (sysSerialPortList != null)
+            {
+                SerialPortList = new(sysSerialPortList);
+            }
+        }
     }
 }
