9d9ea4c14faa4d48899a72d7166d493a7cdbaf61
ives <renhanlinbsl@163.com>
2023-07-12 11:44:43 +0800

    预留部分定义

---
+++ net.serialport.debugger/SerialPortProxyService.Win/Helper/SerialPortHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPortProxyService.Win.Helper
+{
+    public class SerialPortHelper
+    {
+        private SerialPort SerialPort { get; set; }
+
+        public SerialPortHelper()
+        {
+            SerialPort = new SerialPort();
+        }
+    }
+}
---
+++ net.serialport.debugger/SerialPortProxyService.Win/Helper/SocketHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+namespace SerialPortProxyService.Win.Helper
+{
+    public class SocketHelper
+    {
+        private Socket Socket { get; set; }
+
+        public SocketHelper()
+        {
+            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
