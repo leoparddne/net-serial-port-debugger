23a039b51c797a278797100c096cc0de37a83c63
ives <renhanlinbsl@163.com>
2023-07-14 17:54:36 +0800

    主界面接入串口

--- net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
+++ net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
@@ -1,12 +1,17 @@
 ﻿using PropertyChanged;
+using SerialPortProxyService.Win.Helper;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.IO.Ports;
+using System.Text;
 
 namespace SerialPortProxyService.Win.VM
 {
     [AddINotifyPropertyChangedInterface]
     public class VMMain
     {
+        private SerialPortHelper SerialPortHelper;
+
         public ObservableCollection<string> SerialPortList { get; set; }
 
         /// <summary>
@@ -21,6 +26,16 @@ namespace SerialPortProxyService.Win.VM
             {
                 SerialPortList = new(sysSerialPortList);
             }
+
+            SerialPortHelper = new SerialPortHelper("COM2", 9600, Parity.None, 8, StopBits.One);
+            SerialPortHelper.DataReceivedEvent += SerialPortHelper_DataReceivedEvent;
+            SerialPortHelper.Open();
+        }
+
+        private void SerialPortHelper_DataReceivedEvent(byte[] byteData)
+        {
+            var str = Encoding.Default.GetString(byteData, 0, byteData.Length);
+            //File.WriteAllText("./log.txt", str.Trim());
         }
     }
 }
