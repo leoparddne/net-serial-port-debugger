3cfad149eff6e9646863740eaba024f5d4189d8d
ives <renhanlinbsl@163.com>
2023-08-17 16:43:02 +0800

    基础socket收发

--- net.serialport.debugger/SerialPortProxyService.Common/Helper/SocketHelper.cs
+++ net.serialport.debugger/SerialPortProxyService.Common/Helper/SocketHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,39 +9,54 @@ namespace SerialPortProxyService.Common.Helper
         private Encoding Encode { get; set; }
         private Socket SocketInstance { get; set; }
 
-        IPEndPoint ipEndPoint;
-
-        public SocketHelper(Encoding encode, string ip, int port)
+        public SocketHelper(Encoding encode)
         {
-            IPAddress iPAddress = IPAddress.Parse(ip);
-            ipEndPoint = new IPEndPoint(iPAddress, port);
-
             Encode = encode;
             SocketInstance = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
+        public SocketHelper(Encoding encode, Socket socket)
+        {
+            Encode = encode;
+            SocketInstance = socket;
+        }
 
-        public void Send(string msg)
+
+        public int Send(string msg)
         {
             var byteStr = Encode.GetBytes(msg);
 
-            SocketInstance.Send(byteStr);
+            return SocketInstance.Send(byteStr);
         }
 
-        public void Bind()
+        public void Bind(string ip, int port)
         {
+            IPAddress iPAddress = IPAddress.Parse(ip);
+            var ipEndPoint = new IPEndPoint(iPAddress, port);
             SocketInstance.Bind(ipEndPoint);
 
         }
 
         public void Connect(string ip, int port)
         {
+            IPAddress iPAddress = IPAddress.Parse(ip);
+            var ipEndPoint = new IPEndPoint(iPAddress, port);
             SocketInstance.Connect(ipEndPoint);
         }
 
         public void Listen()
         {
             SocketInstance.Listen();
+        }
+
+        public Socket Accept()
+        {
+            return SocketInstance.Accept();
+        }
+
+        public int Receive(byte[] buffer)
+        {
+            return SocketInstance.Receive(buffer);
         }
 
         public void Dispose()
--- net.serialport.debugger/SerialPortProxyService.Test/Program.cs
+++ net.serialport.debugger/SerialPortProxyService.Test/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SerialPortProxyService.Common.Helper;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SerialPortProxyService.Test
 {
@@ -6,7 +10,66 @@ namespace SerialPortProxyService.Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(() =>
+            {
+                StartServer("127.0.0.1", 5000);
+            });
+
+            Task.Run(() =>
+            {
+                StartClient("127.0.0.1", 5000);
+            });
+
+            Console.ReadLine();
+        }
+
+
+        static void StartServer(string ip, int port)
+        {
+            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            var encode = System.Text.Encoding.GetEncoding("GB2312");
+
+            SocketHelper socketHelper = new SocketHelper(encode);
+            socketHelper.Bind(ip, port);
+
+            socketHelper.Listen();
+
+
+            var acceptSocket = socketHelper.Accept();
+
+            SocketHelper acceptSocketHelper = new SocketHelper(encode, acceptSocket);
+
+
+            while (true)
+            {
+                var buffer = new byte[4096];
+                var size = acceptSocketHelper.Receive(buffer);
+                if (size <= 0)
+                {
+                    continue;
+                }
+
+                var str = encode.GetString(buffer);
+                Console.WriteLine($"receive:{str}");
+            }
+        }
+
+        static void StartClient(string ip, int port)
+        {
+            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            var encode = System.Text.Encoding.GetEncoding("GB2312");
+
+            SocketHelper socketHelper = new SocketHelper(encode);
+            socketHelper.Connect(ip, port);
+            while (true)
+            {
+                var sendStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                var size = socketHelper.Send(sendStr);
+                Console.WriteLine($"send:{sendStr}");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
