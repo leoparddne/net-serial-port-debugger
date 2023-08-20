f2b5913274cc37706c1ea84bfab8c4b4d7bda624
ives <renhanlinbsl@163.com>
2023-08-20 18:37:19 +0800

    页面逻辑处理

--- net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
+++ net.serialport.debugger/SerialPortProxyService.Win/VM/VMMain.cs
@@ -1,10 +1,14 @@
-﻿using PropertyChanged;
+﻿using HandyControl.Controls;
+using PropertyChanged;
+using SerialPortProxyService.Common;
+using SerialPortProxyService.Common.Constant;
 using SerialPortProxyService.Common.Helper;
+using SerialPortProxyService.Common.Model;
 using SerialPortProxyService.Win.Common;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.IO.Ports;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 
@@ -13,6 +17,7 @@ namespace SerialPortProxyService.Win.VM
     [AddINotifyPropertyChangedInterface]
     public class VMMain
     {
+        private ProxyAgent proxyAgent;
         public string BtnDisplay { get; set; } = "开始监听";
         public bool ModifyEnable { get; set; } = true;
         private SerialPortHelper SerialPortHelper;
@@ -28,13 +33,22 @@ namespace SerialPortProxyService.Win.VM
         public KeyValuePair<string, int> SelectStopBits { get; set; }
 
 
+        public string IP { get; set; } = "127.0.0.1";
+
+        public int Port { get; set; } = 5000;
+
+        /// <summary>
+        /// 是否为客户端模式
+        /// </summary>
+        public bool ISClientMode { get; set; }
+
         /// <summary>
         /// 选中的串口
         /// </summary>
         public string SelectSerialPort { get; set; }
 
 
-        public int BaudRate { get; set; }
+        public int BaudRate { get; set; } = 9600;
         public Parity Parity { get; set; }
         public int DataBits { get; set; }
         public StopBits StopBits { get; set; }
@@ -51,33 +65,63 @@ namespace SerialPortProxyService.Win.VM
             }
 
             ParityList = EnumHelper.GetEnumInfo(typeof(Parity));
+            SelectParity = ParityList.First();
             StopBitsList = EnumHelper.GetEnumInfo(typeof(StopBits));
-
+            SelectStopBits = StopBitsList.First();
 
             OpenServiceCommand = new BaseCommand((para) =>
             {
-                //Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-                //var encode = System.Text.Encoding.GetEncoding("GB2312");
-                //SerialPortHelper = new SerialPortHelper(encode, "COM2", 9600, Parity.None, 8, StopBits.One);
-                ////SerialPortHelper.DataReceivedEvent += SerialPortHelper_DataReceivedEvent;
-                //SerialPortHelper.ReceiveCallback = SerialPortHelper_DataReceivedEvent;
-                //SerialPortHelper.Open();
+                if (string.IsNullOrEmpty(SelectSerialPort))
+                {
+                    Growl.Error("请选择串口");
+                    return;
+                }
 
-                //var byteStr = encode.GetBytes("test123!@#中文.");
+                proxyAgent = new ProxyAgent();
+                Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                var encode = System.Text.Encoding.GetEncoding("GB2312");
 
-                //SerialPortHelper.Send(byteStr, 0, byteStr.Length);
+                proxyAgent.Build(new ProxyAgentConfig(
+
+                     new NetProxyConfig
+                     {
+                         Encode = encode,
+                         IP = IP,
+                         Port = Port
+                     },
+                     new SerialPortProxyConfig
+                     {
+                         Encode = encode,
+                         PortName = SelectSerialPort,
+                         BaudRate = BaudRate,
+                         Parity = (Parity)SelectParity.Value,
+                         DataBits = DataBits,
+                         StopBits = (StopBits)SelectStopBits.Value
+                     })
+                );
+
+                if (ISClientMode)
+                {
+                    proxyAgent.SwitchMode(RunningModeEnum.Client);
+                }
+                else
+                {
+                    proxyAgent.SwitchMode(RunningModeEnum.Server);
+                }
+
+                if (ModifyEnable)
+                {
+                    proxyAgent.Start();
+                }
+                else
+                {
+                    proxyAgent.Stop();
+                }
 
                 //监听成功后不允许修改
                 ModifyEnable = !ModifyEnable;
                 BtnDisplay = ModifyEnable ? "开始监听" : "停止监听";
             });
-        }
-
-        private void SerialPortHelper_DataReceivedEvent(byte[] byteData)
-        {
-            var str = System.Text.Encoding.GetEncoding("GB2312").GetString(byteData, 0, byteData.Length);
-            //var str = Encoding.UTF8.GetString(byteData, 0, byteData.Length);
-            File.WriteAllText("./log.txt", str.Trim());
         }
     }
 }
