84c0aa15dcc3f21c7d8793a6ca3cfb2adf935832
ives <renhanlinbsl@163.com>
2023-08-26 20:11:21 +0800

    service模式部分代码及配置读取逻辑

--- net.serialport.debugger/SerialPortProxyService/Program.cs
+++ net.serialport.debugger/SerialPortProxyService/Program.cs
@@ -1,9 +1,65 @@
 using SerialPortProxyService;
+using SerialPortProxyService.Common;
+using SerialPortProxyService.Common.Constant;
+using SerialPortProxyService.Common.Model;
+using System.Text;
+
+ConfigurationManager manager = new ConfigurationManager();
+manager.AddJsonFile("appsettings.json");
+
+var config = new ServiceConfig();
+manager.Bind(config);
+
+if (config.Net == null || config.Serial == null)
+{
+    throw new Exception("ÅäÖÃÒì³£");
+}
+
+var proxyAgent = new ProxyAgent();
+Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+var encode = System.Text.Encoding.GetEncoding("GB2312");
+
+proxyAgent.Build(new ProxyAgentConfig(
+
+     new NetProxyConfig
+     {
+         Encode = encode,
+         IP = config.Net.IP,
+         Port = config.Net.Port
+     },
+     new SerialPortProxyConfig
+     {
+         Encode = encode,
+         PortName = config.Serial.PortName,
+         BaudRate = config.Serial.BaudRate,
+         Parity = config.Serial.Parity,
+         DataBits = config.Serial.DataBits,
+         StopBits = config.Serial.StopBits
+     })
+);
+
+switch (config.Mode)
+{
+    case  RunningModeEnum.Client:
+        proxyAgent.SwitchMode(RunningModeEnum.Client);
+        break;
+    case RunningModeEnum.Server:
+        proxyAgent.SwitchMode(RunningModeEnum.Server);
+        break;
+    default:
+        throw new Exception("Running mode error");
+        break;
+}
+
+
+proxyAgent.Start();
+
+
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        //services.AddHostedService<Worker>();
     })
     .Build();
 
---
+++ net.serialport.debugger/SerialPortProxyService/ServiceConfig.cs
@@ -0,0 +1,28 @@
+﻿using SerialPortProxyService.Common.Constant;
+using System.IO.Ports;
+
+namespace SerialPortProxyService
+{
+    public class ServiceConfig
+    {
+        public RunningModeEnum Mode { get; set; }
+        public Net Net { get; set; }
+        public Serial Serial { get; set; }
+    }
+
+    public class Net
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+    }
+
+    public class Serial
+    {
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public Parity Parity { get; set; }
+        public int DataBits { get; set; }
+        public StopBits StopBits { get; set; }
+    }
+
+}
